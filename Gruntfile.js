89108ad4ca185cd20d3451632dbba32e43abe307
rampage <frederik.wolter@googlemail.com>
2016-11-06 14:16:22 +0100

    remove obsolete tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,7 +111,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-merge-json');
-    grunt.loadNpmTasks('grunt-include-source');
     grunt.loadNpmTasks('grunt-contrib-compress');
     
     grunt.registerMultiTask('createMenu', function() {
