4348cc489d5fb8576d3f3760221aec27949897b4
rampage <frederik.wolter@googlemail.com>
2018-08-25 20:07:22 +0200

    Add statistics to map view (area, duration, distance, collisions)

--- src/www/sites/maps/logreader.js
+++ src/www/sites/maps/logreader.js
@@ -44,12 +44,12 @@ define("sites/maps/logreader", [], function() {
             }
              
             var rawTime = values[0];
-            var time =  rawTime.substring(6, 8) + '.' +
-                        rawTime.substring(4, 6) + '.' +
-                        rawTime.substring(0, 4) + ' - ' +
-                        rawTime.substring(08, 10) + ':' +
-                        rawTime.substring(10, 12) + ':' +
-                        rawTime.substring(12, 14);
+            var timeString =    rawTime.substring(0, 4) + '/' +
+                                rawTime.substring(4, 6) + '/' +
+                                rawTime.substring(6, 8) + ' ' +
+                                rawTime.substring(08, 10) + ':' +
+                                rawTime.substring(10, 12) + ':' +
+                                rawTime.substring(12, 14);
             var entryType = values[1];
             var eventType = values[2];
             var info = values[3].trim();
@@ -116,7 +116,7 @@ define("sites/maps/logreader", [], function() {
             return {
                 entry: entryType,
                 type: eventType,
-                time: time,
+                time: Math.round(new Date(timeString).getTime()/1000),
                 previousEvent: previousEvent,
                 pathEvents: pathEvents,
                 position: position,
@@ -157,6 +157,7 @@ define("sites/maps/logreader", [], function() {
             var previousEvents = [];
             var currentRoute = null;
             var numBumps = 0;
+            var distance = 0;
             for (var i = 1; i < logLines.length; i++) {
                 var line = logLines[i];
                 var event = readEvent(line, previousEvents);
@@ -168,6 +169,13 @@ define("sites/maps/logreader", [], function() {
                         logdata.offsets.xMax = Math.max(logdata.offsets.xMax, event.position.x);
                         logdata.offsets.yMin = Math.min(logdata.offsets.yMin, event.position.y);
                         logdata.offsets.yMax = Math.max(logdata.offsets.yMax, event.position.y);
+
+                        var p1 = { x: 0, y: 0 };
+                        if (event.previousEvent !== null && event.previousEvent.entry.indexOf('POS') === 0) {
+                            p1 = event.previousEvent.position;
+                        }
+                        var p2 = event.position;
+                        distance += Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 
                         if (!!currentRoute) {
                             currentRoute.waypoints.push(event.position);
@@ -181,7 +189,7 @@ define("sites/maps/logreader", [], function() {
                             };
                             logdata.routes.push(currentRoute);
                         }
-                        if (event.info === 'Bumping') {
+                        if (event.type === 'Bumping') {
                             numBumps++;
                         }
                     }
@@ -189,14 +197,17 @@ define("sites/maps/logreader", [], function() {
                 }
             }
 
+            var startTime = logdata.events.length > 0 ? logdata.events[0].time : null;
+            var endTime = logdata.events.length > 0 ? logdata.events[logdata.events.length-1].time : null;
+
             logdata.header = {
                 numEvents: logLines.length,
-                startTime: logdata.events.lenght > 0 ? logdata.events[0].time : null,
-                endTime: logdata.events.lenght > 0 ? logdata.events[logdata.events.length-1].time : null,
+                startTime: startTime,
+                endTime: endTime,
                 numBumps: numBumps,
                 status: 'UNKNOWN',
-                duration: 0,
-                travelDistance: 0
+                duration: endTime - startTime,
+                travelDistance: distance / 1000
             };
         }
 
